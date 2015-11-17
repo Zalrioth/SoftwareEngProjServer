ae20f7d0b8da1e672cf3ed1f42717b8dd146edf3
Zalrioth <nicholasbedner@gmail.com>
2015-11-16 20:09:39 -0800

    Multi
    Multi

--- src/gameserver/GameServer.java
+++ src/gameserver/GameServer.java
@@ -4,25 +4,31 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class GameServer {
 
-    public static ArrayList<Player> playerList = new ArrayList<Player>();
+    public static List<Player> playerList;
 
     private static int port = 63400, maxConnections = 10;
 
     // Listen for incoming connections and handle them
     public static void main(String[] args) {
+        playerList = Collections.synchronizedList(new ArrayList<Player>());
         int i = 0;
         int idNum = 0;
+
+        Logic logicLoop = new Logic();
+        
+        Thread logic = new Thread(logicLoop);
+        logic.start();
 
         try {
             ServerSocket listener = new ServerSocket(port);
             Socket server;
 
             while ((i++ < maxConnections) || (maxConnections == 0)) {
-                Player connection;
-
                 server = listener.accept();
                 Player conn_c = new Player(server, idNum);
                 playerList.add(conn_c);
--- src/gameserver/Player.java
+++ src/gameserver/Player.java
@@ -1,25 +1,31 @@
 package gameserver;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 class Player implements Runnable {
+
+    private static int totalPlayers;
 
     private Socket server;
     private String line, input;
     private PrintWriter output;
-    private boolean threadAlive;
+    private JSONObject send;
+    private JSONObject recieve;
 
-    public int idNum;
-    public int xPos;
-    public int yPos;
+    private int idNum;
+    private long xPos;
+    private long yPos;
 
     Player(Socket server, int idNum) {
         this.server = server;
@@ -29,40 +35,120 @@ class Player implements Runnable {
         } catch (IOException ex) {
             Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
         }
-        threadAlive = true;
+        send = new JSONObject();
+        recieve = new JSONObject();
+        input = "";
+        totalPlayers++;
     }
 
     public void run() {
-
-        input = "";
-
         try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            // Get the client message
             while ((input = bufferedReader.readLine()) != null) {
-                int xIndex = input.indexOf("x:");
-                int yIndex = input.indexOf("y:");
-                System.out.println(input);
-                //System.out.println("x: " + xIndex + "y: " + yIndex);
-                xPos = Integer.parseInt(input.substring(xIndex + 2, yIndex));
-                yPos = Integer.parseInt(input.substring(yIndex + 2, input.length()));
-                System.out.println("id: " + idNum + "x: " + xPos + "y: " + yPos);
+                recieveData(input);
+                sendData();
+            }
+            System.out.println("Made it");
+            server.close();
+        } catch (IOException ioe) {
+            System.out.println("Player " + idNum + " Quit");
+            totalPlayers--;
+            GameServer.playerList.remove(this);
+        }
+    }
 
-                for (Player tPlayer : GameServer.playerList) {
-                    if (!tPlayer.equals(this)) {
-                        output.println("id:" + tPlayer.idNum + "x:" + tPlayer.xPos + "y:" + tPlayer.yPos);
-                        System.out.println("Sending player data");
+    public void recieveData(String input) {
+        JSONParser parser = new JSONParser();
+        try {
+            recieve = (JSONObject) parser.parse(input);
+        } catch (ParseException ex) {
+        }
+        xPos = ((Long) recieve.get("xPos")).intValue();
+        yPos = ((Long) recieve.get("yPos")).intValue();
+
+        JSONArray msg = (JSONArray) recieve.get("enemies");
+
+        Iterator<JSONObject> iterator = msg.iterator();
+        while (iterator.hasNext()) {
+            JSONObject updateEnemy = iterator.next();
+            for (Enemy tEnemy : Logic.enemyList) {
+                if ((Long) updateEnemy.get("id") == tEnemy.getID()) {
+                    tEnemy.isAlive = false;
+                }
+            }
+        }
+
+        JSONArray msg2 = (JSONArray) recieve.get("bullets");
+
+        Iterator<JSONObject> iterator2 = msg2.iterator();
+        while (iterator2.hasNext()) {
+            boolean foundBullet = false;
+            JSONObject updateBullet = iterator2.next();
+            for (Bullet tBullet : Logic.bulletList) {
+                if (idNum == tBullet.getPlayerID()) {
+                    //System.out.println(tBullet.getPlayerID());
+                    if ((Long) updateBullet.get("id") == tBullet.getID()) {
+                        foundBullet = true;
+                        tBullet.setAlive((Boolean) updateBullet.get("alive"));
+                        break;
                     }
                 }
             }
 
-            //PrintStream out = new PrintStream(server.getOutputStream());
-            server.close();
-        } catch (IOException ioe) {
-            System.out.println("Player " + idNum + "Quit");
-            threadAlive = false;
-            //System.out.println("IOException on socket listen: " + ioe);
-            //ioe.printStackTrace();
+            if (!foundBullet) {
+                if ((Boolean) updateBullet.get("alive") == false) {
+                } else {
+                    System.out.println("Making new " + idNum + " " + (Long) updateBullet.get("id"));
+                    Bullet newBullet = new Bullet(idNum, ((Long) updateBullet.get("id")).intValue());
+                    Logic.bulletList.add(newBullet);
+                }
+            }
         }
+    }
+
+    public void sendData() {
+        send.clear();
+        send.put("totalPlayers", totalPlayers);
+        JSONArray players = new JSONArray();
+        for (Player tPlayer : GameServer.playerList) {
+            if (!tPlayer.equals(this)) {
+                JSONObject playerDetails = new JSONObject();
+                playerDetails.put("id", tPlayer.idNum);
+                playerDetails.put("xPos", tPlayer.xPos);
+                playerDetails.put("yPos", tPlayer.yPos);
+                playerDetails.put("alive", true);
+                players.add(playerDetails);
+            }
+        }
+        send.put("players", players);
+
+        JSONArray enemies = new JSONArray();
+        for (Enemy tEnemy : Logic.enemyList) {
+            JSONObject enemyDetails = new JSONObject();
+            enemyDetails.put("id", tEnemy.idNum);
+            enemyDetails.put("xPos", tEnemy.getX());
+            enemyDetails.put("yPos", tEnemy.getY());
+            enemyDetails.put("alive", tEnemy.isAlive);
+            enemies.add(enemyDetails);
+        }
+        send.put("enemies", enemies);
+
+        JSONArray bullets = new JSONArray();
+        for (Bullet tBullet : Logic.bulletList) {
+            if (tBullet.playerID != idNum) {
+                //System.out.println("bullet id " + tBullet.idNum);
+                //System.out.println("player id " + tBullet.playerID);
+                //System.out.println("actual id " + idNum);
+                JSONObject bulletDetails = new JSONObject();
+                bulletDetails.put("id", tBullet.idNum);
+                bulletDetails.put("playerID", tBullet.playerID);
+                bulletDetails.put("alive", tBullet.isAlive);
+                bullets.add(bulletDetails);
+            } else {
+                //System.out.println("Not sending");
+            }
+        }
+        send.put("bullets", bullets);
+        output.println(send);
     }
 }
